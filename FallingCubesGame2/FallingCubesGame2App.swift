07db7a4ed14f186dd4fb696462370fdc3eea4f4b
notjaviergui <guisasolis@icloud.com>
2024-09-10 21:37:19 -0400

    Initial Commit

---
+++ FallingCubesGame2/FallingCubesGame2App.swift
@@ -0,0 +1,17 @@
+//
+//  FallingCubesGame2App.swift
+//  FallingCubesGame2
+//
+//  Created by Javier Guisasola on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FallingCubesGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FallingCubesGame2Tests/FallingCubesGame2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FallingCubesGame2Tests.swift
+//  FallingCubesGame2Tests
+//
+//  Created by Javier Guisasola on 9/10/24.
+//
+
+import Testing
+@testable import FallingCubesGame2
+
+struct FallingCubesGame2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
